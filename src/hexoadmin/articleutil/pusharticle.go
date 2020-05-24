57af529393ef517e87d8022d6c5f4c2eda43ba75
edwin <liliang8858@126.com>
2020-05-24 13:14:11 +0800

    update

--- src/hexoadmin/articleutil/pusharticle.go
+++ src/hexoadmin/articleutil/pusharticle.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"os"
 	"strings"
 	"time"
 )
@@ -18,20 +19,26 @@ type RequestInfo struct {
 	DataInterface map[string]interface{}
 }
 type NewPage struct {
-	Title       string        `json:"title"`
-	Author      string        `json:"author"`
-	Content     string        `json:"_content"`
-	Source      string        `json:"source"`
-	Raw         string        `json:"raw"`
-	Slug        string        `json:"slug"`
-	Published   bool          `json:"published"`
-	Date        time.Time     `json:"date"`
-	Updated     time.Time     `json:"updated"`
-	Comments    bool          `json:"comments"`
-	Layout      string        `json:"layout"`
-	Photos      []interface{} `json:"photos"`
-	Link        string        `json:"link"`
-	ID          string        `json:"_id"`
+	Title     string        `json:"title"`
+	Author    string        `json:"author"`
+	Content   string        `json:"_content"`
+	Source    string        `json:"source"`
+	Raw       string        `json:"raw"`
+	Slug      string        `json:"slug"`
+	Published bool          `json:"published"`
+	Date      time.Time     `json:"date"`
+	Updated   time.Time     `json:"updated"`
+	Comments  bool          `json:"comments"`
+	Layout    string        `json:"layout"`
+	Photos    []interface{} `json:"photos"`
+	Link      string        `json:"link"`
+	ID        string        `json:"_id"`
+	Site      struct {
+		Data struct {
+		} `json:"data"`
+	} `json:"site"`
+	Excerpt     string        `json:"excerpt"`
+	More        string        `json:"more"`
 	Path        string        `json:"path"`
 	Permalink   string        `json:"permalink"`
 	FullSource  string        `json:"full_source"`
@@ -82,7 +89,8 @@ type SavePage struct {
 func (this RequestInfo) postUrlEncoded( )([]byte,error){
 	client := &http.Client{}
 	this.Content = strings.Replace(this.Content, "\n", "\\n", -1)
-	fmt.Println("-----33302->",this.Content)
+	fmt.Println("-----33302 url->",this.Url)
+	fmt.Println("-----33302 content->",this.Content)
 
 	req,err := http.NewRequest("POST",this.Url,strings.NewReader(this.Content))
 	if err != nil{
@@ -96,7 +104,7 @@ func (this RequestInfo) postUrlEncoded( )([]byte,error){
 	req.Header.Add("Connection","keep-alive")
 	req.Header.Add("Content-Length","15")
 	req.Header.Add("Content-Type","application/json")
-	req.Header.Add("Cookie","M_distinctid=16f2b92bd2e723-0417a106c9c8c-5f302f45-ff000-16f2b92bd2f878; _ga=GA1.1.2097300613.1584883320; __cfduid=d656753c2d962575c9c82843c051a09451585574191; __gads=ID=333855f8a06f03f4:T=1585912296:S=ALNI_MbkQ7KUzZS7AxY8-tFUi7QNR1o8sg; sm_anonymous_id=2ea93a02-d78d-446d-acdb-22e8c62398df; CNZZDATA1278144647=636958000-1584883305-%7C1586144257; __atuvc=13%7C13%2C31%7C14%2C20%7C15; __atuvs=5e8ab1ca6ef49f37000; connect.sid=s%3A6UiNZWaBVrkxmCyqrnoPgP63CN1au66r.aq9b4JYUFnPbX59WbxYqPQSOOUZNP1Omfx4bdUAY6l0; Hm_lvt_6aa3f11e071bb2936fd0275e7fd3a182=1586137220,1586137235,1586137238,1586148192; Hm_lpvt_6aa3f11e071bb2936fd0275e7fd3a182=1586148192; _ga_M7TMLNPPFB=GS1.1.1586144262.25.1.1586148194.0")
+	req.Header.Add("Cookie","connect.sid=s%3A03tI8Fn5L74RZjq7Gp56vwbN8gdg1UDR.A%2FHNYt%2BGkn4tsAogJQziESp3xLxUlUXF%2B0zaq7X%2FOVc; UM_distinctid=171671a3f995ea-011737edc13156-235b0d4c-ff000-171671a3f9a416; _ga=GA1.1.2118846719.1586572712; sm_anonymous_id=79f8fcc1-81b6-4d62-b726-c29a4a18b688; __gads=ID=1011ae046465c878:T=1586592022:S=ALNI_MZcD3OLIAwjNTqaeOrTtO6d76JcJw; __cfduid=d50fb210c5b397fa9fd9ae663265ca0e91589691213; hibext_instdsigdipv2=1; CNZZDATA1278144647=692246298-1586587467-https%253A%252F%252Fzshipu.coding.net%252F%7C1589890075; __atuvc=1%7C17%2C0%7C18%2C0%7C19%2C0%7C20%2C4%7C21; Hm_lvt_6aa3f11e071bb2936fd0275e7fd3a182=1589892666,1590292425,1590292434,1590292445; Hm_lpvt_6aa3f11e071bb2936fd0275e7fd3a182=1590292445; _ga_M7TMLNPPFB=GS1.1.1590291341.23.1.1590292449.0")
 	req.Header.Add("Host","zshipu.com")
 	req.Header.Add("Origin","https://zshipu.com")
 	req.Header.Add("Referer","https://zshipu.com/admintlg/")
@@ -108,13 +116,18 @@ func (this RequestInfo) postUrlEncoded( )([]byte,error){
 	resp,err := client.Do(req)
 	defer resp.Body.Close()
 	if err != nil{
+		fmt.Println("err client.Do ",err)
 		return nil,err
 	}
+	fmt.Println("ok client.Do ")
 	//读取返回值
 	result,err := ioutil.ReadAll(resp.Body)
 	if err != nil{
+		fmt.Println("err ioutil.ReadAll ",err)
 		return nil,err
 	}
+	fmt.Printf("ok client.ReadAll %s",result)
+	fmt.Printf("ok client.ReadAll %v",result)
 	return result,nil
 }
 
@@ -180,33 +193,96 @@ func saveArticle(someOne NewPage,content string) {
 
 }
 
-func createPage(title string) NewPage {
+func createPage(title string) (NewPage,error) {
 	bd := &RequestInfo{
 		Url:     "https://zshipu.com/admintlg/api/posts/new",
 		Content: "{\"title\":\""+title+"\"}",
 	}
+	var someOne NewPage
 	result, err := bd.postUrlEncoded()
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("createPage bd.postUrlEncoded",err)
+		return someOne,err
 	}
-	var someOne NewPage
+	fmt.Println("string(result)---> ",string(result))
 	if err := json.Unmarshal([]byte(string(result)), &someOne); err == nil {
-		fmt.Println(someOne.ID)
+		fmt.Println("ok someOne.ID",someOne.ID)
 	} else {
-		fmt.Println(err)
+		fmt.Println("err someOne.ID",err)
+		return someOne,err
 	}
-	return someOne
+	return someOne,nil
 }
 
 func PostArticle(param *param.ReqUrlParam) {
 
-	////创建一篇文章
-	someOne := createPage(param.Title)
+	/*
+	/etc/nginx/html/tlg/source/_posts
 
-	////保存文章
-	saveArticle(someOne,param.Content)
+	*/
 
-	////发布文章
-	pushPage(someOne)
+	//1. 查询文件是否存在
+	var path = "/etc/nginx/html/tlg/source/_posts/"+param.Title+".md"
+	exists, err := PathExists(path)
+	if err != nil {
+		return
+	}
+	if exists {
+		path = "/etc/nginx/html/tlg/source/_posts/"+param.Title+"1.md"
+	}
 
+	var content = `
+title: `+param.Title+`
+author: 知识铺
+date: `+GetCurTime()+`
+tags:
+---
+ `+param.Content+`
+`
+	WriteToFile(path,content);
+
+	fmt.Println("save ok !")
+
+	//////创建一篇文章
+	//someOne,err := createPage(param.Title)
+	//if err != nil {
+	//	fmt.Println("PostArticle err",err)
+	//	return
+	//}
+	//////保存文章
+	//saveArticle(someOne,param.Content)
+	//
+	//////发布文章
+	//pushPage(someOne)
+
+}
+
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+func WriteToFile(filepath string,msg string)  {
+	f, err := os.Create(filepath)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	_, err = f.Write([]byte(msg))
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	f.Close()
+}
+
+func GetCurTime() string {
+	timeStr:=time.Now().Format("2006-01-02 15:04:05")
+
+	return timeStr
 }
